751529235c47fe249dd3c2ea897c8ab5d052fe05
xzw <819552192@qq.com>
2024-12-18 09:51:57 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ZegoDynamicImportDemo
+
+## 复现步骤
+
+```bash
+cd zegoDynamicImportDemo
+pnpm i
+pnpm dev
+```
+
+F12在浏览器控制台可以看到报错信息 `TypeError: a.util.Long.fromBits is not a function`
---
+++ zegoDynamicImportDemo/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const zg = ref()
+
+onMounted(async () => {
+  const ZegoExpressEngine = (await import('zego-express-engine-webrtc/esm'))
+    .ZegoExpressEngine
+  zg.value = new ZegoExpressEngine(appID, server)
+  zg.value.setDebugVerbose(false)
+})
+</script>
